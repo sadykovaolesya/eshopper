a3540ee7a0cd612b7833ce7aaec2aaf401d829f7
Olesya <progafox@gmail.com>
2021-04-01 14:54:27 +0500

    Registration

--- authnapp/models.py
+++ authnapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+   
+    avatar = models.ImageField(upload_to="user_avatars", blank=True)
+
+   
--- authnapp/views.py
+++ authnapp/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from django.contrib import auth
 from django.urls import reverse
 
+from authnapp.forms import CustomUserLoginForm, CustomUserRegisterForm
 
-def login(request):
-    return render(request, 'authnapp/login.html')
+def all_auth(request):
+    context = {
+        'login_form': CustomUserLoginForm(),
+        'register_form': CustomUserRegisterForm()
+    }
+
+    return render( request,'authnapp/login.html', context )
+    
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))
 
-# Create your views here.
+def login(request):
+    login_form = CustomUserLoginForm(data = request.POST or None)
+    if request.method == "POST" and login_form.is_valid():
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+    user = auth.authenticate(username=username, password=password)
+    if user and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect(reverse("main"))
+    return HttpResponseRedirect(reverse("all_auth"))
+
+def register(request):
+    
+    if request.method == "POST":
+        register_form = CustomUserRegisterForm(request.POST or None)
+        if  register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse("auth:all_auth"))
+
+    else: 
+        return HttpResponseRedirect(reverse("auth:all_auth"))
